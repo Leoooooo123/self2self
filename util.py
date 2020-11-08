ad982a305abf67341f0ab6073324de6a0d157570
leo <leikei199901@gmail.com>
2020-11-08 17:29:09 +0800

    mask before input

--- util.py
+++ util.py
@@ -4,6 +4,18 @@ import scipy.io as sio
 
 def load_np_image(path, is_scale=True):
     img = cv2.imread(path, -1)
+    b,g,r = cv2.split(img)
+    diff = g-b
+    stain = np.ones(diff.shape,int)
+    ones = np.ones(diff.shape,int)
+    
+    with np.nditer([diff,stain], op_flags=['readwrite']) as it:
+        for x,y in it:
+            if x >=30 and x<=50:
+                y[...]=0
+    mask = np.array([stain,ones,ones])
+    mask = np.transpose(mask,(1,2,0))
+    img = np.multiply(mask,img)
     if img.ndim == 2:
         img = np.expand_dims(img, axis=2)
     img = np.expand_dims(img, axis=0)
